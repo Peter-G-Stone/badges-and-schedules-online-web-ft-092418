000226ec1f01077539cd1046d9b1b60bf3420347
Peter Stone <peter.stone.online@gmail.com>
2018-09-25 22:43:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -18,7 +18,6 @@ def assign_rooms(nameList)
   nameList.each_with_index do |name, index|
     roomsArr.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
   end
-  
   return roomsArr
 end 
 
