f8d256a13a82e8a603be1715ed94d6ab18bc4c41
Peter Stone <peter.stone.online@gmail.com>
2018-09-25 22:00:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,13 +2,21 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end 
 
+
+
 def batch_badge_creator(nameList)
   nameList.each do |name|
     badge_maker(name)    
   end 
 end 
 
+
+
 def assign_rooms(nameList)
-  newListnameList.each do |name|
-    
+  nameList.each_with_index do |name, index|
+    puts "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
 end 
+
+
+
